a09c605f0c2a6d2a0aebdbd22fbb1b463a7e407a
Rishi Pratap <rp0258@srmist.edu.in>
2023-09-08 16:37:51 +0530

    add

--- controller/filter.js
+++ controller/filter.js
@@ -1,36 +1,27 @@
 const filter = async (req, res) => {
-    const val = req.body.data;
+    const val = req.body.data || []; // Default to an empty array if 'data' is missing in the request
   
-    if (Array.isArray(val)) {
-      const response = {
-        is_success: true,
-        user_id: "john_doe_17091999",
-        email: "john@xyz.com",
-        roll_number: "ABCD123",
-        numbers: [],
-        alphabets: [],
-        highest_alphabet: []
-      };
-  
-      val.forEach(item => {
-        if (typeof item === "string" && /^[A-Za-z]$/.test(item)) {
-          response.alphabets.push(item);
-          if (item.toUpperCase() > (response.highest_alphabet[0] || "").toUpperCase()) {
-            response.highest_alphabet = [item];
-          } else if (item.toUpperCase() === (response.highest_alphabet[0] || "").toUpperCase()) {
-            response.highest_alphabet.push(item);
-          }
-        } else if (!isNaN(item)) {
-          response.numbers.push(item);
+    const response = {
+      is_success: true,
+      user_id: "john_doe_17091999",
+      email: "john@xyz.com",
+      roll_number: "ABCD123",
+      numbers: val.filter(item => !isNaN(item)),
+      alphabets: val.filter(item => typeof item === "string" && /^[A-Za-z]$/.test(item)),
+      highest_alphabet: val.reduce((highest, item) => {
+        if (
+          typeof item === "string" &&
+          /^[A-Za-z]$/.test(item) &&
+          (item.toUpperCase() > (highest[0] || "").toUpperCase())
+        ) {
+          return [item];
         }
-      });
+        return highest;
+      }, [])
+    };
   
-      console.log(response);
-      res.send(response);
-    } else {
-      // Handle the case where 'data' is not an array
-      res.status(400).send({ error: "Invalid data format" });
-    }
+    console.log(response);
+    res.send(response);
   };
   
   module.exports = {
